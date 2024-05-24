32b8181bb4b58ad2e7e2b40aa1facb165f23b5c4
AnastasiaKlets <klecz.anastasia@gmail.com>
2024-05-24 16:34:24 +0300

    add images and header

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("header").style.top = "0";
+    } else {
+        document.getElementById("header").style.top = "-100px";
+    }
+    prevScrollpos = currentScrollPos;
+}
 
